10734bfd597d0d6ac948545b23f69d22078f3ce5
Keshav Somani <75680431+keshavs03@users.noreply.github.com>
2023-04-04 04:25:44 +0530

    Created New Branch

--- Speed_Detection/src/App.js
+++ Speed_Detection/src/App.js
@@ -1,25 +1,32 @@
 import React from "react";
-import Navbar from "./components/Navbar";
+import logo from "./logo.svg";
 import "./App.css";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/pages/Home";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Services from "./components/pages/Services";
-import Products from "./components/pages/Products";
+import Services from "./components/Services";
+// import Products from "./components/pages/Products";
 import SignUp from "./components/pages/SignUp";
 import AboutUs from "./components/pages/AboutUs";
+import Upload from "./components/pages/Upload";
+// const BrowserRouter = require("react-router-dom").BrowserRouter;
 
+// const Route = require("react-router-dom").Route;
+
+// const Link = require("react-router-dom").Link;
 function App() {
   return (
     <>
       <Router>
         <Navbar />
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/services" component={Services} />
-          <Route path="/products" component={Products} />
-          <Route path="/sign-up" component={SignUp} />
-          <Route path="/AboutUs" component={AboutUs} />
-        </Switch>
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/services" element={<Services />} />
+          {/* <Route path="/products" element={<Products />} /> */}
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/AboutUs" element={<AboutUs />} />
+          <Route path="/Upload" element={<Upload />} />
+        </Routes>
       </Router>
     </>
   );
