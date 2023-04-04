f4c7b6d7470f9329214751f56a8dafd298ad6b9f
YoNishu <202001024@daiict.ac.in>
2023-04-05 02:00:27 +0530

    Updated Home page and Navbar.js (resolved errors) by Keshav and Nishith

--- Speed_Detection/src/components/Navbar.js
+++ Speed_Detection/src/components/Navbar.js
@@ -44,7 +44,7 @@ function Navbar() {
             </li>
             <li className="nav-item">
               <Link
-                to="/service"
+                to="/Upload"
                 className="nav-links"
                 onClick={closeMobileMenu}
               >
