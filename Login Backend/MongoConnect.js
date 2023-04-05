c8e5eeed717d5cc3e853f1bdfbb4a463cc1256a0
devjadav10 <84441910+devjadav10@users.noreply.github.com>
2023-04-05 11:26:42 +0530

    Added comments in MongoConnect.js

--- Login Backend/MongoConnect.js
+++ Login Backend/MongoConnect.js
@@ -1,11 +1,11 @@
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-dotenv.config();
+const mongoose = require('mongoose'); // Import mongoose
+const dotenv = require('dotenv'); // Import dotenv
+dotenv.config(); // Configure dotenv
 
 
-const connectDB = async () => {
-    await mongoose.connect(process.env.DB_URI, { useNewUrlParser: true });
-    console.log('Connected to MongoDB');
+const connectDB = async () => { // Create a function to connect to MongoDB
+    await mongoose.connect(process.env.DB_URI, { useNewUrlParser: true }); // Connect to MongoDB
+    console.log('Connected to MongoDB'); 
 }
 
-module.exports = connectDB;
+module.exports = connectDB; // Export connectDB function
