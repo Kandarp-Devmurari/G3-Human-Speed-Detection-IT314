31a73ecafd1fcf5a8915e353c17a4883ab57201a
OMVJHAVERI <74951002+OMVJHAVERI@users.noreply.github.com>
2023-04-05 10:37:03 +0530

    Update index.js
    Created Register API, Verifying Fields before signup and added email validator

--- Login Backend/index.js
+++ Login Backend/index.js
@@ -2,50 +2,6 @@ const express = require('express')
 const app = express()
 const connectDB = require('./MongoConnect.js');
 const bodyParser = require('body-parser');
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-const User = require('./Model/User.js');
-
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-});
-
-app.post('/login',async (req,res)=>{
-    const email = req.body.email;
-    const password = req.body.password;
-
-    if(!email || !password){
-        return res.send('Please enter email and password');
-    }
-
-    const userexist = await User.findOne({email: email});
-    if(!userexist){
-        return res.send('User does not exist');
-    }
-
-    if(userexist.password != password){
-        return res.send('Password is incorrect');
-    }
-
-    res.send({
-        "message":'Login successful',
-        "user":userexist
-    });
-
-
-});
-
-app.listen(process.env.PORT, async() => {
-    await connectDB();
-    console.log('Example app listening on port' + process.env.PORT)
-});
-
-const express = require('express')
-const app = express()
-const connectDB = require('./MongoConnect.js');
-const bodyParser = require('body-parser');
 const validator = require('email-validator')
 
 app.use(bodyParser.json());
@@ -122,4 +78,3 @@ app.listen(process.env.PORT, async() => {
     await connectDB();
     console.log('Example app listening on port' + process.env.PORT)
 });
-
