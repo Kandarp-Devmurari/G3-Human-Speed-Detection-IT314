1d73881c73869d9be8b4aee128c0508061aaa465
Kandarp <202001052@daiict.ac.in>
2023-04-26 03:36:40 +0530

    partially automated the process of new .xml generation from the positive, negative user prompt, if the .xml already does not exist

--- speed_detection_algo_2/image_crawler/negative.py
+++ speed_detection_algo_2/image_crawler/negative.py
@@ -1,17 +1,24 @@
-from icrawler.builtin import BingImageCrawler  
+from icrawler.builtin import BingImageCrawler, GoogleImageCrawler  
 import os
+import shutil
 
 
-def negative_image_generator(object,num,img_location):
-    dir = img_location
-    parent_dir = "C:/Users/kanda/Desktop/SE_dataset/"
-    path = os.path.join(parent_dir, dir)
-    os.mkdir(path)
+def negative_image_generator(object,num):
+    path = "C:/Users/kanda/Desktop/SE_dataset/new-dataset"
+    if not os.path.exists(path):
+        os.mkdir(path)
     new_path = os.path.join(path, "n")
-    os.mkdir(new_path)
+
+    if not os.path.exists(new_path):
+        os.mkdir(new_path)
+    else:
+        shutil.rmtree(new_path)
+        os.mkdir(new_path)
 
     classes=object.split(',')
     number=num
     for c in classes:
-        bing_crawler=BingImageCrawler(storage={'root_dir': fr'C:\Users\kanda\Desktop\SE_dataset\{img_location}\n'})
-        bing_crawler.crawl(keyword=c,filters=None,max_num=number,offset=0)            
+        bing_crawler=BingImageCrawler(parser_threads=10, downloader_threads=20,storage={'root_dir': r'C:\Users\kanda\Desktop\SE_dataset\new-dataset\n'})
+        bing_crawler.crawl(keyword=c,filters=None,max_num=number,offset=0)    
+            
+negative_image_generator('human face',100)
