e1b834a04553a9abc25e7a0739bb56cc62d13245
Kandarp <202001052@daiict.ac.in>
2023-04-28 12:04:05 +0530

    added exception error handling logger

--- speed_detection_algo_2/app.py
+++ speed_detection_algo_2/app.py
@@ -14,9 +14,26 @@ import _dlib_pybind11
 from multiprocessing import Process
 from dotenv import load_dotenv
 from flask_cors import CORS
-
+from werkzeug.exceptions import HTTPException
+import logging
+import traceback
 
 app = Flask(__name__) 
+logger = logging.getLogger('werkzeug')
+
+@app.errorhandler(HTTPException)
+def handle_http_exception(error):
+    error_dict = {
+        'code': error.code,
+        'description': error.description,
+        'stack_trace': traceback.format_exc() 
+    }
+    log_msg = f"HTTPException {error_dict.code}, Description: {error_dict.description}, Stack trace: {error_dict.stack_trace}"
+    logger.log(msg=log_msg)
+    response = jsonify(error_dict)
+    response.status_code = error.code
+    return response
+
 CORS(app)
 email_id = ""
 
